c58c093e02277158cb63a649a03f52b32e808ae4
Fill_Kolson <kiril.shew4enko@gmail.com>
2026-03-22 20:04:55 +0100

    chore: add eslint + prettier and fix lint issues

---
+++ eslint.config.js
@@ -0,0 +1,75 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import eslintConfigPrettier from 'eslint-config-prettier';
+
+export default [
+  {
+    ignores: ['dist/**', 'node_modules/**'],
+  },
+  eslintConfigPrettier,
+  {
+    files: ['**/*.{js,jsx}'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: {
+        ...globals.browser,
+      },
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...react.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+      ...reactRefresh.configs.recommended.rules,
+
+      'react/react-in-jsx-scope': 'off',
+
+      eqeqeq: ['error', 'always'],
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
+      'no-debugger': 'error',
+      'no-alert': 'warn',
+      'no-implicit-coercion': 'warn',
+      'no-return-await': 'error',
+      'no-useless-catch': 'error',
+      'no-unsafe-optional-chaining': 'error',
+      'no-shadow': 'error',
+      'prefer-const': 'error',
+      'no-var': 'error',
+      'object-shorthand': ['warn', 'always'],
+      'prefer-template': 'warn',
+      curly: ['error', 'all'],
+      'default-case': 'warn',
+      'no-fallthrough': 'error',
+      'no-else-return': 'warn',
+
+      'no-unused-vars': [
+        'warn',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+        },
+      ],
+
+      'no-trailing-spaces': 'error',
+    },
+  },
+];
